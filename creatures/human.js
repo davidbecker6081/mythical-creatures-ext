bf34b8866e2e96aed35584d54ab364a8ce37ca30
Joe Heitkamp <noetic97@gmail.com>
2017-05-22 11:07:11 -0600

    remove code that passes tests

--- creatures/human.js
+++ creatures/human.js
@@ -1,15 +1,1 @@
-function Human(name) {
-  this.name = name;
-  this.summonCount = 0;
-  this.isSane = true;
-  this.alive = true;
-}
 
-Human.prototype.summonMonster = function (cthulhu) {
-  this.summonCount++;
-  if (this.summonCount === 3) {
-    cthulhu.imprisoned = !cthulhu.imprisoned;
-  }
-};
-
-module.exports = Human;
--- creatures/zombie.js
+++ creatures/zombie.js
@@ -1,18 +1,1 @@
-function Zombie() {
-  this.alive = false;
-  this.name = 'Rob';
-  this.brainPower = 0;
-  this.superZombie = false;
-}
 
-Zombie.prototype.eatBrains = function(human) {
-  if (human.alive === true) {
-    human.alive = false;
-    this.brainPower++;
-    if (this.brainPower >= 5) {
-      this.superZombie = true;
-    }
-  } 
-}
-
-module.exports = Zombie;
